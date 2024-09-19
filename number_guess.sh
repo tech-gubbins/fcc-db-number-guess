251c6207c36b16e47eb400c8696665050faa2b98
Joshua McNeel <joshmcneel@gmail.com>
2024-09-19 19:52:30 +0000

    feat: added welcome messages and queries

--- number_guess.sh
+++ number_guess.sh
@@ -10,9 +10,25 @@ PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
 echo -e "Enter your username: "
 read USERNAME
 
-# Check if usernamne exists
+# Check if username exists
+USERNAME_RESULT=$($PSQL "SELECT username FROM players WHERE username = '$USERNAME'")
 
-# If username doesn't exist, create user in database
+# If username doesn't exist...
+if [[ -z $USERNAME_RESULT ]]
+then
+  # Create username in database
+  RESULT=$($PSQL "INSERT INTO players(username, games_played, best_game) VALUES('$USERNAME', 0, 0)")
+
+  # Print welcome message
+  echo -e "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  # Query database for user's stats
+  USER_STATS=$($PSQL "SELECT games_played, best_game FROM players WHERE username = '$USERNAME'")
+  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_STATS"
+
+  # Print welcome message
+  echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
 
 # Generate random number between 1 and 1000
 
